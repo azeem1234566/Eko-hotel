bdeda71eec2717dac98059b41164812e744b495c
Azeem <lawalazeem945@gmail.com>
2025-08-09 05:32:43 +0100

    eko hotel clone

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+ /** @type {import('tailwindcss').Config} */
+  export default {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        banner1:"rgb(205, 162, 116)",
+        banner2:"rgb(56,60,64)",
+         banner3:"rgb(4,48,64)",
+         text:"rgb(97,135,130)",
+         banner4:"rgb(243, 233, 223)",
+      }
+     
+    },
+  },
+  plugins: [],
+}
